4ea6e16bd00befc55dfdabf90d7fe4cb03991301
Rajarshi Biswas <rajarshibiswas25@gmail.com>
2017-01-19 21:26:32 -0500

    function

--- Project-1/src/project_1.py
+++ Project-1/src/project_1.py
@@ -1,10 +1,27 @@
 import csv
 import sys
 
-f = open(sys.argv[1], 'rt')
-try:
-    reader = csv.reader(f)
-    for row in reader:
-        print row
-finally:
-    f.close()
+
+# Read the data file.
+# fileName - The name of the file.
+def read_data_file (fileName, f):
+
+    try:
+        f = open (fileName, 'rt')
+        # read file
+        reader = csv.reader(f)
+        #print row by row
+        for row in reader:
+            print row[2,0:4]
+    except IOError:
+        print "Could not open file: ", fileName
+    fileName = "hi"
+    # Close the file.
+    if (f):
+        f.close()
+    return
+
+f = []
+# fileName = input("Enter he data set path: ")
+fileName = "../DataSet/Iris.csv"
+read_data_file(fileName, f)
