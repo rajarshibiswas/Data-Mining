9fcd936e41e31be1227ffcda9eaabd0536722aa3
Rajarshi Biswas <rajarshibiswas25@gmail.com>
2017-01-21 17:05:31 -0500

    using panda to read cvs

--- Project-1/src/project_1.py
+++ Project-1/src/project_1.py
@@ -1,46 +1,44 @@
-import csv
-import sys
+import pandas as pd
 
 # Cosine Similarity proximity function.
-def Cosine Similarity(f)
+#def Cosine Similarity(f)
     # Algo for Cosine Similarity
-
-    return
+    #return
 
 # Read the data file.
 # fileName - The name of the file.
 def read_data_file(fileName, f):
     try:
-        f = open (fileName, 'rt')
-        # read file
-        reader = csv.reader(f)
+        #f = open (fileName, 'rt')
+        # read the file
+        data = pd.read_csv(fileName)
         #print row by row
-        for row in reader:
-            print row[2,0:4]
+        #for row in reader:
+        #    print row[2,0:4]
+        print data.head()
     except IOError:
         print "Could not open file: ", fileName
-    fileName = "hi"
-    # Close the file.
-    if (f):
-        f.close()
     return
 
-def analyze_data()
+# the main function
+def analyze_data():
     f = []
     # fileName = input("Enter he data set path: ")
-    fileName = "../DataSet/Iris.csv"
+    fileName = r'../DataSet/Iris.csv'
     read_data_file(fileName, f)
-    print "1. Cosine Similarity"
-    print "2. "
-    choice = input("Enter your choice:")
+#
+#    print "1. Cosine Similarity"
+#    print "2. "
+#    choice = input("Enter your choice:")
 
-    if choice == 1:
+#    if choice == 1:
         # call the particular function
-    ellif choice == 2:
+#    ellif choice == 2:
         #
-    else:
-        print "Error in choosing"
+#    else:
+#        print "Error in choosing"
 
     return
 
+# call the runner function.
 analyze_data()
