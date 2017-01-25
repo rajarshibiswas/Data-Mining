206df5ee54f28fb2b6a09b5520101983a91e9350
Rajarshi Biswas <rajarshibiswas25@gmail.com>
2017-01-24 21:45:59 -0500

    minkowski_distance

--- Project-1/src/project_1.py
+++ Project-1/src/project_1.py
@@ -11,8 +11,7 @@ np.set_printoptions(precision=3)
 
 
 def plot_hist(data):
-    df = data.loc[:, ['sepal_length', 'sepal_width', ' petal_length',
-                      ' petal_width']].copy()
+    df = data.loc[:, ['sepal_length', 'sepal_width', ' petal_length',' petal_width']].copy()
     plt.figure()
     df[' petal_width'].hist()
     df.hist()
@@ -39,6 +38,42 @@ def cosine_similarity(data):
                 cos[i][j] = dotMatrix[i][j]/(np.sqrt(dotMatrix[i][i]) * np.sqrt(dotMatrix[j][j]))
     print cos[1]
     #print data.head()
+    return
+
+def minkowski_distance(data, k, r):
+    num_data_frame_row = data.shape[0]
+    num_data_frame_row = 5
+    # The result array
+    eculidean_dis = []
+
+    for i in range(num_data_frame_row):
+        x = data.iloc[i].values[0:4]
+        temp = []
+        for j in range(num_data_frame_row):
+            y = data.iloc[j].values[0:4]
+            #temp.append(((np.sqrt(np.sum((x - y) ** 2)), j)))
+            temp.append((np.power(np.sum(np.power(np.absolute(x - y), r)), (1.0/r)), j))
+        eculidean_dis.append(temp)
+
+    df = DataFrame(columns=('1st', '1s-pre', '2nd', '2nd-pre', '3rd', '3rd-pre'))
+
+    # print the result
+    for i in range(num_data_frame_row):
+        # sort the tuple based on the euclidean_distance
+        result = np.array(sorted(eculidean_dis[i], key=lambda x: x[0]))
+        # df.loc[i] = [result[0][1:k+1]]
+        l = []
+        for k in range(4):
+            l.append(result[k + 1][0])
+            l.append(result[k + 1][1])
+        df.loc[i] = [l[1], l[0], l[3], l[2], l[5], l[6]]
+        # print result[1][1]
+        # print result[1:k+1]
+    # print result
+    # resultDF = DataFrame(data = result)
+    df.index.name = "ID"
+    print df
+
     return
 
 # Compute euclidean distance
@@ -74,7 +109,7 @@ def euclidean_distance(data, k):
     #resultDF = DataFrame(data = result)
     df.index.name = "ID"
     print df
-    plot_hist(df)
+
 
     return
 
@@ -85,9 +120,18 @@ def read_data_file(fileName):
     try:
         # read the file
         data = pd.read_csv(fileName)
+        data.shape
     except IOError:
         print ("Could not open file: "), fileName
     return data
+
+def normalize_data(data):
+    data = data[['sepal_length','sepal_width', ' petal_length',' petal_width']]
+    print "data max", data.min()
+    #print "data min",data.max()
+    norm_data = (data - data.min()) / (data.max() - data.min())
+    #print norm_data.head()
+    return norm_data
 
 # the main function
 def analyze_data():
@@ -107,8 +151,12 @@ def analyze_data():
         #
 #    else:
 #        print "Error in choosing"
-    #euclidean_distance(data, 4)
-    plot_hist(data)
+    print data.head()
+    data = normalize_data(data)
+    euclidean_distance(data, 4)
+    #plot_hist(data)
+    minkowski_distance(data, 4, 6)
+    #print data.head()
     return
 
 # call the runner function.
