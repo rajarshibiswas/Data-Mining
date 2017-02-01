0bda1190fab7099e43d53aecbd0a9345d6b31fee
gogol2016 <sayamganguly@gmail.com>
2017-01-31 22:15:58 -0500

    Final changes

--- Project-1/src/income.py
+++ Project-1/src/income.py
@@ -5,16 +5,13 @@ from pandas import DataFrame
 import warnings
 
 
-def analyze_data():
+def analyze_data(k):
     warnings.filterwarnings("ignore")
-    k = input("Enter the number of proximities ")
-    filename = '../DataSet/income_tr.csv'
-    # fileName = '..\DataSet\Iris.csv'
+    filename = 'income_tr.csv'
     data = read_data_file(filename)
     data = prepare_data(data)
     euclidean_distance(data, k+1)
     cosine_similarity(data, k+1)
-    #plot_data(data)
 
 
 def read_data_file(fileName):
@@ -115,7 +112,7 @@ def euclidean_distance(data, k):
             y = data.iloc[j].values[0:11]
             temp.append((np.sqrt(np.sum((x - y) ** 2)), j))
         eculidean_dis.append(temp)
-    prepare_output(eculidean_dis, k, '../DataSet/Euclidean_Income.csv', False)
+    prepare_output(eculidean_dis, k, 'Income_Euclidean.csv', False)
 
 
 def prepare_output(distance_matrix, k, filename, similarity_flag):
@@ -157,7 +154,7 @@ def cosine_similarity(df, k):
             else:
                 temp.append((0.0, j))
         cos.append(temp)
-    prepare_output(cos, k, '../DataSet/Cosine_Income.csv', True)
+    prepare_output(cos, k, 'Income_Cosine.csv', True)
 
 
 
@@ -189,44 +186,4 @@ def prepare_data(data):
 
     return data
 
-"""
-    dict_edu, dict_work_class, dict_mar_status, dict_occupation, \
-    dict_relationship, dict_race, dict_gender, dict_country = prepare_dict(data)
-    convert_to_class(data, 'education', dict_edu)
-    convert_to_class(data, 'workclass', dict_work_class)
-    convert_to_class(data, 'marital_status', dict_mar_status)
-    convert_to_class(data, 'occupation', dict_occupation)
-    convert_to_class(data, 'relationship', dict_relationship)
-    convert_to_class(data, 'race', dict_race)
-    convert_to_class(data, 'gender', dict_gender)
-    convert_to_class(data, 'native_country', dict_country)
-def prepare_dict(data):
-    dict_edu = make_dictionary(data, 'education')
-    dict_work_class = make_dictionary(data, 'workclass')
-    dict_mar_status = make_dictionary(data, 'marital_status')
-    dict_occupation = make_dictionary(data, 'occupation')
-    dict_relationship = make_dictionary(data, 'relationship')
-    dict_race = make_dictionary(data, 'race')
-    dict_gender = make_dictionary(data, 'gender')
-    dict_country = make_dictionary(data, 'native_country')
-    return dict_edu, dict_work_class, dict_mar_status,dict_occupation,\
-           dict_relationship,dict_race,dict_gender,dict_country
-def make_dictionary(data,colName):
-    d = dict(zip(data[colName].unique(), np.arange(0, data[colName].unique().size)))
-    return d
-def convert_to_class(data,colName,d):
-    #data = data['education'].where(data['education'] == d.items()[0][0]).dropna()
-    for item in d.items():
-        data.loc[data[colName] == item[0], colName] = item[1]
-"""
-
-def plot_data(data):
-    plt.figure()
-    #plt.scatter(data['education'],data['class'])
-    data.occupation.value_counts().plot(kind='bar')
-    #df = data.loc[:, ['education', 'class']].copy()
-    #data['education'].hist()
-    plt.show()
-
-
-analyze_data()
+analyze_data(k)
